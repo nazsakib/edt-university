c697e3bbf95bd95bff3e8ebc86095c2598cac11a
nazsakib <sakibsnaz@gmail.com>
2024-05-21 10:58:05 +0600

    event updated

--- front-page.php
+++ front-page.php
@@ -13,11 +13,41 @@
 </div>
 
 <div class="full-width-split group">
+
+    <!-- homepage events -->
     <div class="full-width-split__one">
         <div class="full-width-split__inner">
             <h2 class="headline headline--small-plus t-center">Upcoming Events</h2>
 
-            <div class="event-summary">
+            <?php
+
+            $homePageEvents = new WP_Query(
+                array(
+                    'posts_per_page' => 2,
+                    'post_type' => 'event'
+                )
+            );
+
+            while ($homePageEvents->have_posts()) {
+                $homePageEvents->the_post(); ?>
+                <div class="event-summary">
+                    <a class="event-summary__date t-center" href="<?php the_permalink(); ?>">
+                        <span class="event-summary__month"><?php the_time('M'); ?></span>
+                        <span class="event-summary__day"><?php the_time('d'); ?></span>
+                    </a>
+                    <div class="event-summary__content">
+                        <h5 class="event-summary__title headline headline--tiny"><a
+                                href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
+                        <p><?php echo wp_trim_words(get_the_content(), 20); ?> <a href="<?php the_permalink(); ?>"
+                                class="nu gray">Learn more</a>
+                        </p>
+                    </div>
+                </div>
+            <?php }
+
+            ?>
+
+            <!-- <div class="event-summary">
                 <a class="event-summary__date t-center" href="#">
                     <span class="event-summary__month">Mar</span>
                     <span class="event-summary__day">25</span>
@@ -38,11 +68,13 @@
                     <p>Live music, a taco truck and more can found in our third annual quad picnic day. <a href="#"
                             class="nu gray">Learn more</a></p>
                 </div>
-            </div>
+            </div> -->
 
             <p class="t-center no-margin"><a href="#" class="btn btn--blue">View All Events</a></p>
         </div>
     </div>
+
+    <!-- homepage blogs -->
     <div class="full-width-split__two">
         <div class="full-width-split__inner">
             <h2 class="headline headline--small-plus t-center">From Our Blogs</h2>
