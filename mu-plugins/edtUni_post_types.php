7d573afc6e0c1c6b193f844929e44553d818a977
nazsakib <sakibsnaz@gmail.com>
2024-05-22 00:45:54 +0600

    program post type added

--- mu-plugins/edtUni_post_types.php
+++ mu-plugins/edtUni_post_types.php
@@ -3,25 +3,59 @@
 // custom post type
 function edtUni_post_types()
 {
+    // event post type
     $labels = array(
         'name' => 'Events',
         'add_new_item' => 'Add New Event',
         'edit_item' => 'Edit Event',
         'all_items' => 'All Events',
-        'singular_name' => 'Event' 
+        'singular_name' => 'Event'
     );
-    $args = array (
-    	'labels' => $labels,
-     	'public' => true,
-    	'has_archive' => true,
-     	'menu_icon' => 'dashicons-calendar',
-    	'rewrite' => array(
-    		'slug' => 'events'
-    	),
-    	'taxonomies' => array(
-    		'category', 'post_tag'
-    	),
-    	'show_in_rest' => true,
+    $args = array(
+        'labels' => $labels,
+        'public' => true,
+        'has_archive' => true,
+        'menu_icon' => 'dashicons-calendar',
+        'show_in_rest' => true,
+        'rewrite' => array(
+            'slug' => 'events'
+        ),
+        'taxonomies' => array(
+            'category',
+            'post_tag'
+        ),
+        'supports' => array(
+            'title',
+            'editor',
+            'author',
+            'thumbnail',
+            'excerpt'
+        )
+    );
+    register_post_type('event', $args);
+
+
+    //program post type
+    $labels = array(
+        'name' => 'Programs',
+        'add_new_item' => 'Add New Program',
+        'edit_item' => 'Edit Program',
+        'all_items' => 'All Programs',
+        'singular_name' => 'Program'
+    );
+    $args = array(
+        'labels' => $labels,
+        'public' => true,
+        'has_archive' => true,
+        'menu_icon' => 'dashicons-awards',
+        'show_in_rest' => true,
+        'rewrite' => array(
+            'slug' => 'programs'
+        ),
+        'taxonomies' => array(
+            'category',
+            'post_tag'
+        ),
         'supports' => array(
             'title',
             'editor',
@@ -29,9 +63,8 @@ function edtUni_post_types()
             'thumbnail'
         )
     );
-    register_post_type('event', $args);
+    register_post_type('program', $args);
 }
 add_action('init', 'edtUni_post_types');
 
 ?>
-         	
