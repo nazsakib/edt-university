53ec81342fb827c9cec64a12b234507cbdf1de1e
nazsakib <sakibsnaz@gmail.com>
2024-05-24 21:55:42 +0600

    page banner reusable code

--- functions.php
+++ functions.php
@@ -1,5 +1,24 @@
 <?php
 
+// pagebanner code
+function pageBanner()
+{ ?>
+
+    <div class="page-banner">
+        <div class="page-banner__bg-image" style="background-image: url(<?php $pageBannerImage = get_field('page_banner_bg_image');
+        echo $pageBannerImage['sizes']['pageBanner']; ?>);">
+        </div>
+        <div class="page-banner__content container container--narrow">
+            <h1 class="page-banner__title"><?php the_title(); ?></h1>
+            <div class="page-banner__intro">
+                <p><?php the_field('page_banner_subtitle'); ?></p>
+            </div>
+        </div>
+    </div>
+
+<?php }
+
+
 //  styles and scripts
 function edtUni_files()
 {
