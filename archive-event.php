c843b921ce9686920be1a04a20740e09e4d6516c
nazsakib <sakibsnaz@gmail.com>
2024-05-25 06:55:04 +0600

    archive event, program page updated

--- archive-event.php
+++ archive-event.php
@@ -1,19 +1,14 @@
-<?php get_header(); ?>
+<?php get_header();
 
-<!-- banner -->
-<div class="page-banner">
-    <div class="page-banner__bg-image"
-        style="background-image: url(<?php echo get_theme_file_uri('/images/ocean.jpg') ?>);">
-    </div>
-    <div class="page-banner__content container container--narrow">
-        <h1 class="page-banner__title">
-            All Events
-        </h1>
-        <div class="page-banner__intro">
-            <p>See what is going on our world!</p>
-        </div>
-    </div>
-</div>
+pageBanner(
+    array(
+        'title' => 'All Events',
+        'subtitle' => 'See what is coming next and what we did',
+        'photo' => ''
+    )
+);
+
+?>
 
 <!-- container -->
 <div class="container container--narrow page-section">
--- archive-program.php
+++ archive-program.php
@@ -1,19 +1,18 @@
 <?php get_header(); ?>
 
-<!-- banner -->
-<div class="page-banner">
-    <div class="page-banner__bg-image"
-        style="background-image: url(<?php echo get_theme_file_uri('/images/ocean.jpg') ?>);">
-    </div>
-    <div class="page-banner__content container container--narrow">
-        <h1 class="page-banner__title">
-            All Programs
-        </h1>
-        <div class="page-banner__intro">
-            <p>Choose which subject you want to study!</p>
-        </div>
-    </div>
-</div>
+<?php
+
+pageBanner(
+    array(
+
+        'title' => 'All Programs',
+        'subtitle' => 'Choose which one do you like most as your death weapon!',
+        'photo' => ''
+
+    )
+);
+
+?>
 
 <!-- container -->
 <div class="container container--narrow page-section">
