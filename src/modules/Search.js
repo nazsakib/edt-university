c92b827d2cdc59447188d165bdd88bc84d3bb212
nazsakib <sakibsnaz@gmail.com>
2024-06-01 11:36:34 +0600

    minor fixed

--- src/modules/Search.js
+++ src/modules/Search.js
@@ -6,6 +6,7 @@ class Search {
         this.openButton = $(".js-search-trigger");
         this.closeButton = $(".search-overlay__close");
         this.searchOverlay = $(".search-overlay");
+        this.fullBody = $(body);
         this.events();
     }
 
@@ -13,14 +14,20 @@ class Search {
     events() {
         this.openButton.on("click", this.openOverlay.bind(this));
         this.closeButton.on("click", this.closeOverlay.bind(this));
+        $(document).on("keyup", this.keyPressFunction.bind(this));
     }
 
     // 3. methods ( function, action )
     openOverlay() {
         this.searchOverlay.addClass("search-overlay--active");
+        this.fullBody.addClass("body-no-scroll");
     }
     closeOverlay() {
         this.searchOverlay.removeClass("search-overlay--active");
+        $("body").removeClass("body-no-scroll");
+    }
+    keyPressFunction() {
+        console.log("hello");
     }
 }
 
